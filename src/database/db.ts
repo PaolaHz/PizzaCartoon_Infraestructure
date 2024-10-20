43eecbbac959a0ec1ffbf273bf00c868aaed55b3
JD93Lopez <juandavid.jdlea@gmail.com>
2024-10-19 23:29:50 -0500

    Produccion

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
 
 export const connection = mysql.createPool({
-  host: 'localhost',
+  host: '10.0.133.43',
   user: 'root',
-  password: 'root',
+  password: 'root1234',
   database: 'infraestructura',
   waitForConnections: true,
   connectionLimit: 10,
