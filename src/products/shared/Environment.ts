8f305db48f182d6fdce4ecb48cb2d7e2fb0df633
JD93Lopez <juandavid.jdlea@gmail.com>
2024-10-20 18:38:13 -0500

    Correcion link imagenes

--- src/products/shared/Environment.ts
+++ src/products/shared/Environment.ts
@@ -1,6 +1,6 @@
 export default class Environment {
   public static readonly getDomain = (): string => {
-    const HOST = process.env["HOST"] ?? 'internal-LB-Privadas-222101674.us-east-1.elb.amazonaws.com'
+    const HOST = process.env["HOST"] ?? 'lb-proyect-1850608966.us-east-1.elb.amazonaws.com'
     const PROTOCOL = process.env["PROTOCOL"] ?? 'http'
     return `${PROTOCOL}://${HOST}`
   }
