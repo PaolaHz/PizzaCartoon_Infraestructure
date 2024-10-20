d5b84a923824866bb4b254e79257c9c868df764d
JD93Lopez <juandavid.jdlea@gmail.com>
2024-10-20 17:46:23 -0500

    Cambio usuario db

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,8 +2,8 @@ import mysql from 'mysql2/promise';
 
 export const connection = mysql.createPool({
   host: '10.0.133.43',
-  user: 'root',
-  password: 'root1234',
+  user: 'usuariodb',
+  password: 'user1234',
   database: 'infraestructura',
   waitForConnections: true,
   connectionLimit: 10,
